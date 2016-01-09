013ed98f436fca92edb7c2df6f5b25284706f0d7
Justin <klofseattle@hotmail.com>
2016-01-09 10:29:03 -0800

    first assgnt

--- class-01-jquery-and-the-dom/pair-assignment/justin-miklean/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/justin-miklean/scripts/article.js
@@ -8,22 +8,24 @@ function Article (opts) {
   this.title = opts.title;
   this.category = opts.category;
   this.body = opts.body;
-  this.pubDate = opts.pubDate;
+  this.publishedOn = opts.publishedOn;
 };
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   // 1/7/16
 
-  $newArticle.data('category', this.category);
-  $newArticle.data('body', this.body);
-  $newArticle.data('pubDate',this.pubDate);
-  $newArticle.data('author',this.author);
-  $newArticle.data('authorUrl',this.authorUrl);
-
-
   // Include the publication date as a 'title' attribute to show on hover:
+  $newArticle.attr('data-category', this.category);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('h1').html(this.title);
+  //$newArticle.find('publishedOn',this.publishedOn);
+  $newArticle.find('a').text(this.author);
+
+  $newArticle.find('a').attr('href',this.authorUrl);
+
+
 
   // Display the date as a relative number of "days ago":
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -32,7 +34,7 @@ Article.prototype.toHtml = function() {
 
   // 1/7/16 removing class of function or className specific??? or anon fx?
 
-  $newArticle.removeClass();
+  $newArticle.removeClass('template');
   return $newArticle;
 };
 
