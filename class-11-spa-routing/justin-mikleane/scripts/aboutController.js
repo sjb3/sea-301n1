19c622dde6ba651b52d6e58eeefd153a95be4aca
Justin <klofseattle@hotmail.com>
2016-02-16 18:58:27 -0800

    HW11

---
+++ class-11-spa-routing/justin-mikleane/scripts/aboutController.js
@@ -0,0 +1,13 @@
+(function(module) {
+  var aboutController = {};
+
+  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.index = function() {
+	// $('#article').hide();    
+    $('#main>section').hide;
+    $('#about').show;
+
+  };
+
+  module.aboutController = aboutController;
+})(window);
